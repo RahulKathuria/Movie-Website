28f24aac7efaf51ae00dfd89d043b8eae8acaba9
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-08-08 14:27:51 +0530

    comments added for function

--- media.py
+++ media.py
@@ -3,9 +3,25 @@ import webbrowser
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+    """ This function contains the details of the movie.
+    Args:
+    	self:It points on a particluar movie.
+    	movie_title:represents movie title.
+    	movie_story_line:represents the movie story line.
+    	poster_image:represents the poster image.
+    	trailer_youtube:represents the youtube trailer of the particluar movie.
+    	Returns :
+    	It returns the movie title, movie storyline, poster image and the youtube trailer of the movie.
+    """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
     def show_trailer(self):
+    	"""
+    	This function is used to play the trailer of the movie.
+ 	 Args:
+    	self:It points on the particluar trailer which is to be played.
+    	Returns: It returns the youtube trailer of the particluar movie.
+    	"""
         webbrowser.open(self.trailer_youtube_url)
