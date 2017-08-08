98c4081e593d44a69c15a92f28beafd6217f3a9a
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-08-08 18:03:08 +0530

    indentation corrected

--- media.py
+++ media.py
@@ -3,8 +3,9 @@ import webbrowser
 class Movie():
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-    """ This function contains the details of the movie.
-    Args:
+    	"""
+    	This function contains the details of the movie.
+    	Args:
     	self:It points on a particluar movie.
     	movie_title:represents movie title.
     	movie_story_line:represents the movie story line.
@@ -12,7 +13,7 @@ class Movie():
     	trailer_youtube:represents the youtube trailer of the particluar movie.
     	Returns :
     	It returns the movie title, movie storyline, poster image and the youtube trailer of the movie.
-    """
+ 		"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -20,8 +21,8 @@ class Movie():
     def show_trailer(self):
     	"""
     	This function is used to play the trailer of the movie.
- 	 Args:
+ 	 	Args:
     	self:It points on the particluar trailer which is to be played.
     	Returns: It returns the youtube trailer of the particluar movie.
     	"""
-        webbrowser.open(self.trailer_youtube_url)
+    	webbrowser.open(self.trailer_youtube_url)
